67370e7143b85a127dfe5e6de9f2c9dba592d8dd
Gabri00s <gabri00fj@live.it>
2024-08-11 16:08:51 +0200

    Update MatchController.java

--- LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/controller/MatchController.java
+++ LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/controller/MatchController.java
@@ -1,8 +1,11 @@
 package it.unipi.lsmsd.LSMSD_Project.controller;
 
 import it.unipi.lsmsd.LSMSD_Project.model.Match;
+import it.unipi.lsmsd.LSMSD_Project.model.User;
 import it.unipi.lsmsd.LSMSD_Project.service.MatchService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.http.ResponseEntity;
@@ -16,28 +19,55 @@ public class MatchController {
     private MatchService matchService;
 
     @PostMapping("/add")
-    public ResponseEntity<Match> addMatch(@RequestBody Match match) {
-        Match newMatch = matchService.addMatch(match);
-        return ResponseEntity.ok(newMatch);
+    public ResponseEntity<?> addMatch(@RequestBody Match match, HttpSession session) {
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser != null) {
+            Match newMatch = matchService.addMatch(match);
+            return ResponseEntity.ok(newMatch);
+        } else {
+            return new ResponseEntity<>("Operazione non autorizzata", HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<Match>> getAllMatches() {
-        List<Match> matches = matchService.getAllMatches();
-        if (!matches.isEmpty()) {
-            return ResponseEntity.ok(matches);
+    public ResponseEntity<?> getAllMatches(HttpSession session) {
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser != null && currentUser.isAdmin()) {
+            List<Match> matches = matchService.getAllMatches();
+            if (!matches.isEmpty()) {
+                return ResponseEntity.ok(matches);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
         } else {
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>("Operazione non autorizzata", HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/getByUser")
-    public ResponseEntity<List<Match>> getMatchesByUser(@RequestParam String user) {
-        List<Match> matches = matchService.getMatchesByUser(user);
-        if (!matches.isEmpty()) {
-            return ResponseEntity.ok(matches);
+    public ResponseEntity<?> getMatchesByUser(@RequestParam(required = false) String user, HttpSession session) {
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser == null) {
+            return new ResponseEntity<>("User not authenticated", HttpStatus.UNAUTHORIZED);
+        }
+        if (user == null) {
+            List<Match> matches = matchService.getMatchesByUser(currentUser.getUsername());
+            if (!matches.isEmpty()) {
+                return ResponseEntity.ok(matches);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
         } else {
-            return ResponseEntity.notFound().build();
+            if (currentUser.isAdmin()) {
+                List<Match> matches = matchService.getMatchesByUser(user);
+                if (!matches.isEmpty()) {
+                    return ResponseEntity.ok(matches);
+                } else {
+                    return ResponseEntity.notFound().build();
+                }
+            } else {
+                return new ResponseEntity<>("Operazione non autorizzata", HttpStatus.UNAUTHORIZED);
+            }
         }
     }
 }
