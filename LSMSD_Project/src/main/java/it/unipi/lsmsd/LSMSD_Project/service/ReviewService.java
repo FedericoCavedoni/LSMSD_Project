b1b572249b397450451a916fa27cfd054ed2ba21
FedericoCavedoni <fedecave2000@gmail.com>
2024-08-11 11:01:13 +0200

    Recent reviews API

--- LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/service/ReviewService.java
+++ LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/service/ReviewService.java
@@ -3,6 +3,10 @@ package it.unipi.lsmsd.LSMSD_Project.service;
 import it.unipi.lsmsd.LSMSD_Project.dao.ReviewRepository;
 import it.unipi.lsmsd.LSMSD_Project.model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +16,9 @@ public class ReviewService {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public Review addReview(Review review) {
         String username = review.getUsername();
@@ -50,5 +57,14 @@ public class ReviewService {
 
     public List<Review> findReviewByUserAndGame(String username, String game) {
         return reviewRepository.findByUsernameAndGame(username, game);
+    }
+
+    public List<Review> getRecentReviews(String game, int limit) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("game").is(game));
+        query.with(Sort.by(Sort.Direction.DESC, "date"));
+        query.limit(limit);
+
+        return mongoTemplate.find(query, Review.class);
     }
 }
