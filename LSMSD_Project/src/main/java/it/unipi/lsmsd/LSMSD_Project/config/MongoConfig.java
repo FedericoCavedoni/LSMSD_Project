643131b9176f7760253c99c7b2896f0124e45b82
FedericoCavedoni <fedecave2000@gmail.com>
2024-08-22 16:31:01 +0200

    Update mongoDB Clustering

--- LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/config/MongoConfig.java
+++ LSMSD_Project/src/main/java/it/unipi/lsmsd/LSMSD_Project/config/MongoConfig.java
@@ -1,7 +1,14 @@
 package it.unipi.lsmsd.LSMSD_Project.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver;
@@ -9,6 +16,23 @@ import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 public class MongoConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                //.applyConnectionString(new com.mongodb.ConnectionString("mongodb://localhost:27017/Progetto"))
+                .applyConnectionString(new com.mongodb.ConnectionString("mongodb://localhost:27018/Progetto"))
+                .writeConcern(WriteConcern.W1)
+                .readPreference(ReadPreference.nearest())
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoClient(), "Progetto"));
+    }
 
     @Bean
     public MappingMongoConverter mappingMongoConverter(MongoMappingContext context) {
